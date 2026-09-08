5ba4e88fcf6aea6f8cf708f292f4ec49ed85058b
WebRTC <yadavroushan8269@gmail.com>
2026-09-08 19:14:57 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.BM_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAN5jFZ0hBOb-Zmbrv8W1E2aGqnmd8_iOM",
-  authDomain: "bitcoin-minning-web.firebaseapp.com",
-  projectId: "bitcoin-minning-web",
-  storageBucket: "bitcoin-minning-web.firebasestorage.app",
-  messagingSenderId: "691683594489",
-  appId: "1:691683594489:web:96252fee18748e54ad9228"
-};
